3d6859aca09b3324e14057e0a52333455f24b33f
Anur85 <an-nv85@yandex.ru>
2024-09-16 22:09:11 +0500

    add mock and entities

---
+++ src/shared/entities.types.ts
@@ -0,0 +1,39 @@
+export interface IUser {
+  name: string;
+  email: string;
+  avatar: string;
+  password: string;
+  userType: string;
+}
+
+export interface IComments {
+  comment: string;
+  date: string;
+  rating: number;
+  author: IUser;
+}
+
+export interface Location {
+  latitude: number;
+  longitude: number;
+}
+
+export interface IOffer {
+  title: string;
+  description: string;
+  date: string;
+  city: string;
+  previewImage: string;
+  images: string[];
+  isPremium: boolean;
+  isFavorite: boolean;
+  rating: number;
+  type: string;
+  bedrooms: number;
+  quests: number;
+  price: number;
+  goods: string[];
+  host: IUser;
+  comments: number;
+  location: Location;
+}
