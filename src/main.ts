4ad7a786a34b14c23761b41591ac51568bdfc053
Anur85 <an-nv85@yandex.ru>
2024-09-18 21:11:37 +0500

    add shebang

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
-import { CLIApplication, HelpCommand, ImportCommand, VersionCommand } from './cli/index.js';
+#!/usr/bin/env node
+import {CLIApplication, HelpCommand, ImportCommand, VersionCommand} from './cli/index.js';
 
 function bootstrap() {
   const cliApplication = new CLIApplication();
-  cliApplication.registerCommands([
-    new HelpCommand(),
-    new VersionCommand(),
-    new ImportCommand(),
-  ]);
+  cliApplication.registerCommands([new HelpCommand(), new VersionCommand(), new ImportCommand()]);
 
   cliApplication.processCommand(process.argv);
 }
