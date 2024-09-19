c4278af29fd46c2800c2e0a9972b4084924f944e
Anur <an-nv85@yandex.ru>
2024-09-19 09:09:28 +0500

    fix lint error

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -11,13 +11,13 @@ export class HelpCommand implements Command {
       chalk.green(
         `
         Программа для подготовки данных для REST API сервера.
-        ${chalk.blue.bold(`Пример:`)}
-          ${chalk.blue.underline(` cli.js --<command> [--arguments]`)}
+        ${chalk.blue.bold('Пример:')}
+          ${chalk.blue.underline(' cli.js --<command> [--arguments]')}
         Команды:
-            --version:                   ${chalk.yellow(`# выводит номер версии`)}
-            --help:                      ${chalk.yellow(`# печатает этот текст`)}
-            --import <path>:             ${chalk.yellow(`# импортирует данные из TSV`)}
-            --generate <n> <path> <url>  ${chalk.yellow(`# генерирует произвольное количество тестовых данных`)}
+            --version:                   ${chalk.yellow('# выводит номер версии')}
+            --help:                      ${chalk.yellow('# печатает этот текст')}
+            --import <path>:             ${chalk.yellow('# импортирует данные из TSV')}
+            --generate <n> <path> <url>  ${chalk.yellow('# генерирует произвольное количество тестовых данных')}
     `
       )
     );
