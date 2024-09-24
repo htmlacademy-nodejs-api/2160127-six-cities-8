dc3369c2be68350e58c37a9ce88c832730d0ab3b
Anur85 <an-nv85@yandex.ru>
2024-09-24 16:50:18 +0500

    use got in GenerateCommand

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,15 +1,37 @@
+import got from 'got';
+
 import {ICommand} from './command.interface.js';
+import {MockServerDataType} from 'src/shared/types/mock-server-data.type.js';
 
 export class GenerateCommand implements ICommand {
+  private initialData: MockServerDataType;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
   public getName(): string {
     return '--generate';
   }
 
-  public execute(...parameters: string[]): void {
+  public async execute(...parameters: string[]): Promise<void> {
     const [count, filepath, url] = parameters;
     const offerCount = Number.parseInt(count, 10);
 
     // Код для получения данных с сервера.
     // Формирование объявлений.
+    try {
+      await this.load(url);
+    } catch (error: unknown) {
+      console.error('Can\'t generate data');
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 }
