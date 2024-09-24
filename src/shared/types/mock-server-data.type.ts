d326ec5f0d92a0fc008befe6abfd88171782efa5
Anur85 <an-nv85@yandex.ru>
2024-09-24 15:01:33 +0500

    add MockServerData type

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,11 @@
+export type MockServerData = {
+  title: string[];
+  description: string[];
+  city: string[];
+  previewImage: string[];
+  images: string[];
+  features: string[];
+  users: string[];
+  emails: string[];
+  avatars: string[];
+};
