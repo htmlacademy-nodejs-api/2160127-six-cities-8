ed81df192869c4dbaa5f6176d47dbf4ea180f3a8
Anur85 <an-nv85@yandex.ru>
2024-10-01 19:03:20 +0500

    bind DatabaseClient

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker compose  --file ./docker-compose.dev.yml  --env-file ./.env  --project-name "buy-and-sell" --httpinterface up -d
+docker compose  --file ./docker-compose.dev.yml  --env-file ./.env  --project-name "buy-and-sell" up -d
--- src/main.rest.ts
+++ src/main.rest.ts
@@ -3,9 +3,9 @@ import { Container } from 'inversify';
 
 import { ILogger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
-
 import { IConfig, RestConfig, RestSchema } from './shared/libs/config/index.js';
 import { Component } from './shared/types/component.enum.js';
+import { IDatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
 
 
 async function bootstrap() {
@@ -13,7 +13,7 @@ async function bootstrap() {
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<ILogger>(Component.Logger).to(PinoLogger).inSingletonScope();
   container.bind<IConfig<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
-
+  container.bind<IDatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
   const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
 }
