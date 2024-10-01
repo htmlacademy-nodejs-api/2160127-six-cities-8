7503308baf2111455cc7ae6590c2ebf4786f3024
Anur85 <an-nv85@yandex.ru>
2024-10-01 22:37:29 +0500

    fix UserService

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,20 +1,16 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
 
-import { ILogger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
-import { IConfig, RestConfig, RestSchema } from './shared/libs/config/index.js';
 import { Component } from './shared/types/component.enum.js';
-import { IDatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
+import { createRestApplicationContainer } from './rest/rest.container.js';
+import { createUserContainer } from './shared/modules/user/index.js';
 
 
 async function bootstrap() {
-  const container = new Container();
-  container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
-  container.bind<ILogger>(Component.Logger).to(PinoLogger).inSingletonScope();
-  container.bind<IConfig<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
-  container.bind<IDatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
-  const application = container.get<RestApplication>(Component.RestApplication);
+  const appContainer = Container.merge(createRestApplicationContainer(), createUserContainer(),);
+
+  const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
 }
 
