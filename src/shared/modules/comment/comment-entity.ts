2a5a66c989ff801279ac3a7835a42cfe445a39e5
Anur85 <an-nv85@yandex.ru>
2024-10-09 20:51:58 +0500

    Реализует обновлённый интерфейс OfferService.

--- src/shared/modules/comment/comment-entity.ts
+++ src/shared/modules/comment/comment-entity.ts
@@ -6,10 +6,12 @@ import {
   Ref,
 } from '@typegoose/typegoose';
 import { UserEntity } from './../user/user.entity.js';
+import { IOfferEntity } from './../offer/offer.entity.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface CommentEntity extends defaultClasses.Base {}
-// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+
+
 @modelOptions({
   schemaOptions: {
     collection: 'Comments',
@@ -18,20 +20,21 @@ export interface CommentEntity extends defaultClasses.Base {}
 })
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class CommentEntity extends defaultClasses.TimeStamps {
-  @prop({ required: true })
-  public date: Date;
-
-  @prop({
-    ref: UserEntity,
-    required: true,
-  })
-  public userId: Ref<UserEntity>;
-
-  @prop({ required: true })
+  @prop({required: true, trim: true})
   public comment: string;
 
-  @prop({ required: true })
+  @prop({required: true, trim: true})
+  public date: Date;
+
+  @prop({required: true, trim: true})
   public rating: number;
+
+  @prop({ ref: IOfferEntity, required: true })
+  public offerId: Ref<IOfferEntity>;
+
+  @prop({ ref: UserEntity, required: true })
+  public userId: Ref<UserEntity>;
+
 }
 
 export const CommentModel = getModelForClass(CommentEntity);
--- src/shared/modules/offer/default-offer-service.ts
+++ src/shared/modules/offer/default-offer-service.ts
@@ -1,9 +1,11 @@
 import { ILogger } from '../../libs/logger/index.js';
-import { Component } from '../../types/index.js';
-import { types } from '@typegoose/typegoose';
+import { Component, SortType } from '../../types/index.js';
+import { DocumentType, types } from '@typegoose/typegoose';
 import { inject, injectable } from 'inversify';
 import { CreateOfferDto } from './index.js';
 import { IOfferEntity } from './index.js';
+import { UpdateOfferDto } from './dto/update-offer.dto.js';
+import { DEFAULT_OFFER_COUNT } from './offer.constant.js';
 import {
   OfferEntityDocument,
   IOfferService,
@@ -27,6 +29,56 @@ export class DefaultOfferService implements IOfferService {
   findById(offerId: string): Promise<OfferEntityDocument | null> {
     return this.offerModel
       .findById(offerId)
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async find(count: number = DEFAULT_OFFER_COUNT, offset: number = 0): Promise<DocumentType<IOfferEntity>[]> {
+    return this.offerModel
+      .find({}, {}, { limit: count, skip: offset })
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async deleteById(offerId: string): Promise<DocumentType<IOfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndDelete(offerId)
+      .exec();
+  }
+
+  public async updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<IOfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, dto, {new: true})
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async exists(documentId: string): Promise<boolean> {
+    return (await this.offerModel
+      .exists({_id: documentId})) !== null;
+  }
+
+  public async incCommentCount(offerId: string): Promise<DocumentType<IOfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, {'$inc': {
+        commentCount: 1,
+      }}).exec();
+  }
+
+  public async findNew(count: number): Promise<DocumentType<IOfferEntity>[]> {
+    return this.offerModel
+      .find()
+      .sort({ createdAt: SortType.Down })
+      .limit(count)
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async findDiscussed(count: number): Promise<DocumentType<IOfferEntity>[]> {
+    return this.offerModel
+      .find()
+      .sort({ commentCount: SortType.Down })
+      .limit(count)
       .populate(['userId'])
       .exec();
   }
