089eb7bcb018eedbb43e7239fa0ecf40699870ac
Anur85 <an-nv85@yandex.ru>
2024-10-13 22:30:39 +0500

    create BaseController

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -1,2 +1,4 @@
 export { HttpMethod } from './types/http-method.enum.js';
 export { IRoute } from './types/route.interface.js';
+export { IController } from './controller/controller.interface.js';
+export { BaseController } from './controller/base-controller.abstract.js';
