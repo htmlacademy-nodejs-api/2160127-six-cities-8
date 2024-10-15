d2ef712525ddf6af4df2a6e5c2f3b2580cf1a43a
Anur85 <an-nv85@yandex.ru>
2024-10-15 21:17:07 +0500

    add NextFunction

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -4,4 +4,6 @@ export { DefaultUserService } from './default-user.service.js';
 export { createUserContainer } from './user.container.js';
 export { IUserService } from './user-service.interface.js';
 export { UserRdo } from './rdo/user.rdo.js';
+export { UserController } from './user.controller.js';
+
 
--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'inversify';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 import { BaseController, HttpMethod } from '../../libs/rest/index.js';
@@ -8,6 +8,7 @@ import { Component } from '../../types/index.js';
 import { IUserService } from './user-service.interface.js';
 import { fillDTO } from '../../helpers/index.js';
 import { UserRdo, CreateUserDto } from './index.js';
+// import { CreateUserRequest } from './create-user-request.type.js';
 
 @injectable()
 export class UserController extends BaseController {
@@ -20,7 +21,7 @@ export class UserController extends BaseController {
     this.logger.info('Register routes for UserController...');
 
     this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
-    this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
+    this.addRoute({ path: '/register', method: HttpMethod.Post, handler: this.create });
   }
 
   public async index(_req: Request, res: Response): Promise<void> {
@@ -32,22 +33,23 @@ export class UserController extends BaseController {
 
   public async create(
     { body }: Request<Record<string, unknown>, Record<string, unknown>, CreateUserDto>,
-    res: Response
+    res: Response, next: NextFunction
   ): Promise<void> {
+    try {
+      const existUser = await this.userService.findByEmail(body.email);
 
-    const existUser = await this.userService.findByEmail(body.email);
-
-    if (existUser) {
-      const existUserError = new Error(`User with email «${body.email}» exists.`);
-      this.send(res,
-        StatusCodes.UNPROCESSABLE_ENTITY,
-        { error: existUserError.message }
-      );
-
-      return this.logger.error(existUserError.message, existUserError);
+      if (existUser) {
+        const existUserError = new Error(`User with email «${body.email}» exists.`);
+        this.send(res,
+          StatusCodes.UNPROCESSABLE_ENTITY,
+          { error: existUserError.message }
+        );
+        return this.logger.error(existUserError.message, existUserError);
+      }
+      const result = await this.userService.create(body, 'secret');
+      this.created(res, fillDTO(UserRdo, result));
+    } catch (error) {
+      return next(error);
     }
-
-    const result = await this.userService.create(body, 'secret');
-    this.created(res, fillDTO(UserRdo, result));
   }
 }
