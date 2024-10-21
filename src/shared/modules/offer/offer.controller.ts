a383b3adccfbe6fc78f7279c4f2ea3e3381f59ee
Anur85 <an-nv85@yandex.ru>
2024-10-21 21:29:12 +0500

    add update offer

--- src/shared/modules/offer/offer.controller.ts
+++ src/shared/modules/offer/offer.controller.ts
@@ -10,6 +10,7 @@ import { ParamOfferId } from './type/param-offerid.type.js';
 import { fillDTO } from '../../helpers/index.js';
 import { OfferRdo } from './index.js';
 import { CreateOfferRequest } from './type/create-offer-request.type.js';
+import { UpdateOfferDto } from './index.js';
 
 
 @injectable()
@@ -26,6 +27,7 @@ export class OfferController extends BaseController {
     this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
     this.addRoute({ path: '/:offerId', method: HttpMethod.Get, handler: this.show });
     this.addRoute({ path: '/:offerId', method: HttpMethod.Delete, handler: this.delete });
+    this.addRoute({ path: '/:offerId', method: HttpMethod.Patch, handler: this.update });
   }
 
   public async index(_req: Request, res: Response): Promise<void> {
@@ -69,6 +71,20 @@ export class OfferController extends BaseController {
     }
 
     this.noContent(res, offer);
+  }
+
+  public async update({ body, params }: Request<ParamOfferId, unknown, UpdateOfferDto>, res: Response): Promise<void> {
+    const updatedOffer = await this.offerService.updateById(params.offerId, body);
+
+    if (!updatedOffer) {
+      throw new HttpError(
+        StatusCodes.NOT_FOUND,
+        `Offer with id ${params.offerId} not found.`,
+        'OfferController'
+      );
+    }
+
+    this.ok(res, fillDTO(OfferRdo, updatedOffer));
   }
 
 }
